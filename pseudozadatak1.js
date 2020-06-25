4cabcf6997e57358d990a32bae266df535ce377f
Maria Holjevac <35504553+holjevacmaria@users.noreply.github.com>
2020-06-25 13:10:45 +0200

    Add files via upload

---
+++ pseudozadatak1.js
@@ -0,0 +1,12 @@
+function palindrom(str) {
+ var regex = /[^A-Za-z0-9]/g;
+ str = str.toLowerCase().replace(regex,'');
+ var len = str.length;
+ for (var i = 0; i < len/2; i++) {
+   if (str[i] !== str[len - 1 - i]) {
+       return false;
+   }
+ }
+ return true;
+}
+palindrom("Ana VOLI Milovana");
